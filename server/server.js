ae367cdbbb220f01f2d5e766fe10c5a3a709e0f5
Purple Persia <navid@navids-imac.mynetworksettings.com>
2024-08-25 09:42:50 -0400

    server file change

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
-const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -16,21 +15,20 @@ const server = new ApolloServer({
 
 const startApolloServer = async () => {
   await server.start();
-
-  app.use(express.urlencoded({ extended: false }));
+  
+  app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
+  
+  app.use('/graphql', expressMiddleware(server));
 
-  app.use('/graphql', expressMiddleware(server, {
-    context: authMiddleware
-  }));
-
+  // if we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
-  }
+  } 
 
   db.once('open', () => {
     app.listen(PORT, () => {
